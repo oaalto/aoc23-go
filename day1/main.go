8e8b55071e3fef4a1a6f684a932b097140dc55ee
Olli Aalto <oaalto@gmail.com>
2024-04-06 17:59:02 +0300

    remove unnecessary initialization

--- day1/main.go
+++ day1/main.go
@@ -12,11 +12,11 @@ type numberParser interface {
 }
 
 func main() {
-	var part1 part1Parser = 0
+	var part1 part1Parser
 	calculate("./day1/input.txt", &part1)
 	fmt.Println("Day 1 part 1: ", part1)
 
-	var part2 part2Parser = 0
+	var part2 part2Parser
 	calculate("./day1/input.txt", &part2)
 	fmt.Println("Day 1 part 2: ", part2)
 }
